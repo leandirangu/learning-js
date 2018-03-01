451afa8387ebe700da02acc5c758cc30ef9f17c5
Leah Wamuyu <leah.wamuyu@yahoo.com>
2018-03-01 11:35:16 +0300

    fifth commit

---
+++ SwitchCase.js
@@ -0,0 +1,15 @@
+var isSunday = false
+
+switch(isSunday){
+	case true:
+	console.log(true)
+	break;
+	case false:
+	console.log("Today is not sunday")
+	break;
+	default:
+	console.log("error")
+	break;
+
+
+}
---
+++ SwitchCaseNumber.js
@@ -0,0 +1,37 @@
+var dayOfTheWeek = "saturday"
+
+switch(dayOfTheWeek){
+	case "Monday":
+	console.log("Today is monday")
+	break;
+
+	case "Tuesday":
+	console.log("Today is  Tuesday")
+	break;
+
+	case "wednesday":
+	console.log("Today is  wednesday")
+	break;
+
+	case "Thursday":
+	console.log("Today is  Thursday")
+	break;
+
+	case "friday":
+	console.log("Today is  friday")
+	break;
+
+	case "saturday":
+	console.log("Today is  saturday")
+	break;
+
+	case "sunday":
+	console.log("Today is  sunday")
+	break;
+
+	default:
+	console.log("error")
+	break;
+
+
+}
