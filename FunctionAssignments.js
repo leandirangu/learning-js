0baaec3d9232023b838576d5e95672d88cccbdab
Leah Wamuyu <leah.wamuyu@yahoo.com>
2018-03-08 12:29:10 +0300

    added functions

---
+++ FunctionAssignments.js
@@ -0,0 +1,39 @@
+function divide (){
+	var NumberOne=12;
+	var NumberTwo=3;
+	var answer=NumberOne / NumberTwo;
+	return answer;
+}
+divide();
+var answer= divide();
+console.log(answer)
+
+function subtract (){
+	var NumberOne=10;
+	var NumberTwo=2;
+	var answer=NumberOne - NumberTwo;
+	return answer;
+}
+subtract();
+var answer= subtract();
+console.log(answer)
+
+function multiply (){
+	var NumberOne=8;
+	var NumberTwo=7;
+	var answer=NumberOne * NumberTwo;
+	return answer;
+}
+multiply();
+var answer= multiply();
+console.log(answer)
+
+function add (){
+	var NumberOne=12;
+	var NumberTwo=3;
+	var answer=NumberOne + NumberTwo;
+	return answer;
+}
+add();
+var answer= add();
+console.log(answer)
---
+++ Functions.js
@@ -0,0 +1,18 @@
+// this is a void function
+function add(){
+	var numberOne =10;
+	var numberTwo =20;
+	var sum= numberOne + numberTwo;
+	console.log(sum);
+}
+// call the function
+ function multiply(){
+ 	var numberOne = 3;
+ 	var numberTwo = 4;
+ 	var numberThree= 5;
+ 	var answer= numberOne * numberTwo * numberThree;
+ 	return (answer)
+ }
+add()
+var result = multiply()
+console.log(result)
