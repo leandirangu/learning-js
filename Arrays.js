f24cb2445135ddf8234764862e79ce10fcdb5efc
Leah Wamuyu <leah.wamuyu@yahoo.com>
2018-03-10 12:04:29 +0300

    added arrays and parameters

---
+++ Arrays.js
@@ -0,0 +1,8 @@
+var student=["Jane", "Mary", "Lauren", "Ivy", "Fatma", "Judith", "Irene"]
+var shoeSize=[7, 8, 5, 6, 23]
+
+// get the length of the array
+// console.log(student.length)
+
+for(i=0;i<student.length;i+=1)
+	console.log(student[i]);
---
+++ FunctionParameters.js
@@ -0,0 +1,18 @@
+function AreaOfTriangle(base, height){
+
+	var answer= 0.5 * base * height;
+	return answer
+}
+var answer=AreaOfTriangle(30,10);
+
+var result=AreaOfTriangle(32,16);
+
+var x=AreaOfTriangle(20,13);
+
+	print(answer)
+	print(result)
+	print(x)
+	
+ function print(answer){
+ 	console.log(" Area of Triangle: " + answer)
+ }
