7e99310278b4930886eef941ab96e6d4896ba50d
Leah Wamuyu <leah.wamuyu@yahoo.com>
2018-06-22 12:34:02 +0300

    added arrowfunction

---
+++ arrowFunction.js
@@ -0,0 +1,8 @@
+var addition=(x,y,z) => x+y+z;
+console.log(addition(4,8,9));
+
+var subtraction=(m,n,l) => m-n-l;
+console.log(subtraction(9,6,1));
+
+var multiply=(r,s,t) => r*s*t;
+console.log(multiply(3,5,6))
